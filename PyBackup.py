782ec090bdb3176f36fbb9054093245d32682d6c
saschahoeche <sascha.hoeche@gmx.com>
2023-05-29 10:14:40 +0200

    First functional draft

---
+++ PyBackup.py
@@ -0,0 +1,128 @@
+import os
+import shutil
+import time
+
+
+def incremental_copy(source_folder, destination_folder):
+    """
+    Perform an incremental copy of a folder and its contents to a specified location.
+
+    Args:
+        source_folder (str): The path to the source folder.
+        destination_folder (str): The path to the destination folder.
+
+    Returns:
+        None
+
+    Raises:
+        None
+    """
+    # Ensure source folder exists
+    if not os.path.exists(source_folder):
+        print(f"Source folder '{source_folder}' does not exist.")
+        return
+
+    # Ensure destination folder exists
+    if not os.path.exists(destination_folder):
+        os.makedirs(destination_folder)
+
+    # Get the total number of files for progress calculation
+    total_files = sum(len(files) for _, _, files in os.walk(source_folder))
+
+    # Initialize progress variables
+    processed_files = 0
+    copied_files = 0
+    start_time = time.time()
+
+    # List to store the copied files' information
+    copied_files_info = []
+
+    # Iterate over the source folder recursively
+    for root, dirs, files in os.walk(source_folder):
+        # Calculate the corresponding destination folder
+        dest_root = root.replace(source_folder, destination_folder, 1)
+
+        # Ensure the corresponding destination folder exists
+        if not os.path.exists(dest_root):
+            os.makedirs(dest_root)
+
+        # Copy modified or new files to the destination
+        for file in files:
+            src_file = os.path.join(root, file)
+            dest_file = os.path.join(dest_root, file)
+
+            # Copy the file if it's modified or doesn't exist in the destination
+            if not os.path.exists(dest_file) or (
+                os.path.getmtime(src_file) > os.path.getmtime(dest_file)
+            ):
+                shutil.copy2(src_file, dest_file)
+                copied_files += 1
+
+                # Add copied file's information to the list
+                copied_files_info.append((src_file, dest_file))
+
+            # Update progress variables
+            processed_files += 1
+
+            # Calculate progress percentage
+            progress = processed_files / total_files * 100
+
+            # Calculate elapsed time
+            elapsed_time = time.time() - start_time
+
+            # Print progress information
+            print(
+                f"Progress: {progress:.2f}% | "
+                f"Processed Files: {processed_files}/{total_files} | "
+                f"Copied Files: {copied_files} | "
+                f"Elapsed Time: {elapsed_time:.2f}s",
+                end="\r",
+            )
+
+    print(
+        f"\nIncremental copy completed successfully. Total Files Copied: {copied_files}."
+    )
+
+    # Generate log file
+    generate_log_file(copied_files_info, destination_folder)
+
+
+def generate_log_file(copied_files_info, destination_folder):
+    """
+    Generate a log file with the date and time of the incremental copy, containing
+    a summary of the copied files and their source-destination directories.
+
+    Args:
+        copied_files_info (list): List of tuples containing the copied files' source
+                                  and destination information.
+        destination_folder (str): The path to the destination folder.
+
+    Returns:
+        None
+
+    Raises:
+        None
+    """
+    log_filename = os.path.join(
+        os.path.dirname(destination_folder),
+        f"incremental_copy_log_{os.path.basename(destination_folder)}_{time.strftime('%Y%m%d_%H%M%S')}.txt",
+    )
+
+    with open(log_filename, "w") as log_file:
+        log_file.write("Incremental Copy Log\n")
+        log_file.write(f"Date and Time: {time.strftime('%Y-%m-%d %H:%M:%S')}\n\n")
+
+        log_file.write("Copied Files:\n")
+        for src_file, dest_file in copied_files_info:
+            log_file.write(f"From: {src_file}\n")
+            log_file.write(f"To: {dest_file}\n")
+            log_file.write("\n")
+
+    print(f"Log file '{log_filename}' generated.")
+
+
+if __name__ == "__main__":
+    source_folder = "/path/to/source/folder"
+    destination_folder = "/path/to/destination/folder"
+
+    incremental_copy(source_folder, destination_folder)
---
+++ README.md
@@ -0,0 +1,50 @@
+# Incremental Folder Backup
+
+Author: Sascha Höche
+
+## Overview
+
+This script performs an incremental copy of a folder and its contents to a specified location. It identifies modified or new files in the source folder and copies them to the corresponding destination folder. The script keeps track of the copied files and generates a log file with the summary of the copied files' source and destination directories.
+
+## Features
+
+- Performs incremental backup of a folder and its contents
+- Supports copying files and preserving their metadata (timestamps, permissions, etc.)
+- Generates a log file with the summary of the copied files
+- Provides progress information during the copying process
+- Does not require any external libraries
+
+## Usage
+
+1. Ensure you have Python 3 installed on your system.
+2. Clone this repository or download the `incremental_copy.py` script.
+3. Open a terminal or command prompt and navigate to the directory containing the `incremental_copy.py` script.
+4. Modify the source folder and destination folder paths in the `incremental_copy.py` script according to your requirements.
+5. Run the script by executing the following command:
+
+   ```shell
+   python incremental_copy.py
+   ```
+6. The script will perform the incremental copy, display the progress information, and generate a log file in the parent folder of the destination folder.
+
+## Example
+
+    ```python
+    import os
+    import shutil
+    import time
+
+    # Function definitions...
+
+    if __name__ == "__main__":
+        source_folder = "/path/to/source/folder"
+        destination_folder = "/path/to/destination/folder"
+
+        incremental_copy(source_folder, destination_folder)
+    ```
+
+## License
+This project is licensed under the MIT License.
+
+
+
