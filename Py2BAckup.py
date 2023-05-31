b1d801633661646d50d47c8c9929c58a18d123aa
saschahoeche <saschahoeche@gmx.net>
2023-05-31 18:46:20 +0200

    Py2 version fixed

--- Py2BAckup.py
+++ Py2BAckup.py
@@ -19,7 +19,7 @@ def incremental_copy(source_folder, destination_folder):
     """
     # Ensure source folder exists
     if not os.path.exists(source_folder):
-        print("Source folder '%s' does not exist." % source_folder)
+        print "Source folder '%s' does not exist." % source_folder
         return
 
     # Ensure destination folder exists
@@ -39,6 +39,9 @@ def incremental_copy(source_folder, destination_folder):
     # List to store the copied files' information
     copied_files_info = []
 
+    # Retrieve the last backup time if available
+    last_backup_time = get_last_backup_time(destination_folder)
+
     # Iterate over the source folder recursively
     for root, dirs, files in os.walk(source_folder):
         # Calculate the corresponding destination folder
@@ -53,9 +56,11 @@ def incremental_copy(source_folder, destination_folder):
             src_file = os.path.join(root, file)
             dest_file = os.path.join(dest_root, file)
 
-            # Copy the file if it's modified or doesn't exist in the destination
+            # Copy the file if it's modified or doesn't exist in the destination or last backup time is earlier
             if not os.path.exists(dest_file) or (
                 os.path.getmtime(src_file) > os.path.getmtime(dest_file)
+            ) or last_backup_time is None or (
+                os.path.getmtime(src_file) > last_backup_time
             ):
                 shutil.copy2(src_file, dest_file)
                 copied_files += 1
@@ -73,17 +78,60 @@ def incremental_copy(source_folder, destination_folder):
             elapsed_time = time.time() - start_time
 
             # Print progress information
-            print(
-                "Progress: %.2f%% | Processed Files: %d/%d | Copied Files: %d | Elapsed Time: %.2fs" %
-                (progress, processed_files, total_files, copied_files, elapsed_time)
-            )
+            print "Progress: %.2f%% | Processed Files: %d/%d | Copied Files: %d | Elapsed Time: %.2fs" % (
+                progress, processed_files, total_files, copied_files, elapsed_time),
 
-    print(
-        "\nIncremental copy completed successfully. Total Files Copied: %d." % copied_files
-    )
+    print "\nIncremental copy completed successfully. Total Files Copied: %d." % copied_files
 
     # Generate log file
     generate_log_file(copied_files_info, destination_folder)
+
+    # Update the last backup time
+    update_last_backup_time(destination_folder)
+
+
+def get_last_backup_time(destination_folder):
+    """
+    Retrieve the last backup time from the destination folder.
+
+    Args:
+        destination_folder (str): The path to the destination folder.
+
+    Returns:
+        last_backup_time (float): The last backup time as a floating-point timestamp,
+                                  or None if the last backup time file is not found.
+
+    Raises:
+        None
+    """
+    last_backup_file = os.path.join(destination_folder, "last_backup_time.txt")
+
+    if os.path.exists(last_backup_file):
+        with open(last_backup_file, "r") as f:
+            last_backup_time = float(f.read().strip())
+        return last_backup_time
+
+    return None
+
+
+def update_last_backup_time(destination_folder):
+    """
+    Update the last backup time in the destination folder.
+
+    Args:
+        destination_folder (str): The path to the destination folder.
+
+    Returns:
+        None
+
+    Raises:
+        None
+    """
+    last_backup_file = os.path.join(destination_folder, "last_backup_time.txt")
+    current_time = time.time()
+
+    with open(last_backup_file, "w") as f:
+        f.write(str(current_time))
 
 
 def generate_log_file(copied_files_info, destination_folder):
@@ -117,7 +165,7 @@ def generate_log_file(copied_files_info, destination_folder):
             log_file.write("To: %s\n" % dest_file)
             log_file.write("\n")
 
-    print("Log file '%s' generated." % log_filename)
+    print "Log file '%s' generated." % log_filename
 
 
 if __name__ == "__main__":
